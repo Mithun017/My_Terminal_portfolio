73cc656a08d82f825611c9d34eda9718a73f139e
Mithun017 <mithun.m2023ai-ds@sece.ac.in>
2025-11-25 23:33:27 +0530

    Update the background animations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,12 @@
 'use client';
 
 import { Terminal } from '@/components/terminal/Terminal';
-import { Scene } from '@/components/canvas/Scene';
+
 
 export default function Home() {
   return (
-    <main className="min-h-screen w-full relative bg-black">
-      {/* 3D Scene Layer - Z-Index 0 */}
-      <div className="fixed inset-0 z-0">
-        <Scene />
-      </div>
-
-      {/* UI Layer - Z-Index 10 */}
-      <div className="relative z-10 pointer-events-none">
-        <Terminal />
-      </div>
+    <main className="min-h-screen w-full relative">
+      <Terminal />
     </main>
   );
 }
